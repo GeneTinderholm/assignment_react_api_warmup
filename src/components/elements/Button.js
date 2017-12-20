4cfa9afb05b8c253178c312fb07bc10045e1d9f9
GeneTinderholm <tinderholmgene@gmail.com>
2017-12-20 12:19:24 -0600

    demo works

---
+++ src/components/elements/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({type, color, children}) => {
+  return(
+    <button type={type} className={`btn btn-${color}`}>
+      {children}
+    </button>
+  )
+}
+
+export default Button;
